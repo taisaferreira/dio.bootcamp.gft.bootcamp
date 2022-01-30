6fb426735910099e089e4974b9b8bd6fa3fbfbea
taisaferreira <taisaferreiradejesus@gmail.com>
2022-01-29 21:59:54 -0300

    Abstração de um bootcamp usando POO em Java

---
+++ src/dominio/Curso.java
@@ -0,0 +1,27 @@
+package dominio;
+
+public class Curso extends Conteudo {
+    private int cargaHoraria;
+
+    public double calcularXp() {
+        return 10.0D * (double) this.cargaHoraria;
+    }
+
+    public Curso() {
+    }
+
+    public int getCargaHoraria() {
+        return this.cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String toString() {
+        String var10000 = this.getTitulo();
+        return "Curso{titulo='" + var10000 + "', descricao='" + this.getDescricao() + "', cargaHoraria=" + this.cargaHoraria + "}";
+    }
+}
+
+
